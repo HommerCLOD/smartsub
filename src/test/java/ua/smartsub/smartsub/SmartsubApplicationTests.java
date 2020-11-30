52c2c709a544ccbb783573f6edc3c032350ea0e0
NotQuestions <kipen.vasyl@gmail.com>
2020-11-30 14:02:17 +0200

    -30.11.2020

---
+++ src/test/java/ua/smartsub/smartsub/SmartsubApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.smartsub.smartsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartsubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
