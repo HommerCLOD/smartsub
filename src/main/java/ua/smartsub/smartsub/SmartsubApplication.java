38e09b1858699cdfc68355a6ffff8e527ddc1698
NotQuestions <kipen.vasyl@gmail.com>
2020-12-23 00:36:05 +0200

    -create DB MySQL

--- src/main/java/ua/smartsub/smartsub/SmartsubApplication.java
+++ src/main/java/ua/smartsub/smartsub/SmartsubApplication.java
@@ -2,6 +2,7 @@ package ua.smartsub.smartsub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class SmartsubApplication {
